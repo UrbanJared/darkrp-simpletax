09dd57fab725ac2afd75dbbc625d09f0cba0c991
CrazyPraetorian <38169333+CrazyPraetorian@users.noreply.github.com>
2018-04-09 15:58:34 +1000

    Shortened if statements

--- simpletax/sv_taxes.lua
+++ simpletax/sv_taxes.lua
@@ -9,7 +9,8 @@ simpleTax_notifyOnCooldown = false
 
 function getTaxCommand(ply, txt)
 	local text = string.lower(txt)
-	if string.sub(text, 1, 7) == "/settax" or string.sub(text, 1, 7) == "!settax" then
+	local cmd = string.sub(text, 1, 7)
+	if cmd == "/settax" or cmd == "!settax" then
 		if ply:isMayor() then
 			if tonumber(string.sub(text, 9)) then
 				local proposedTax = tonumber(string.sub(text, 9)) 
@@ -32,34 +33,29 @@ function getTaxCommand(ply, txt)
 			DarkRP.notify(ply, 1, 4, "You need to be the mayor to change taxes!")
 			return ""
 		end
-	end
-	if text == "/gettax" or text == "!gettax" then
+	elseif cmd == "/gettax" or cmd == "!gettax" then
 		DarkRP.notify(ply, 0, 4, "Tax is currently " .. simpleTax_totalTax .. "%.")
 		return ""
 	end
 end
 function getTax(ply, oldsalary) --Tax the citizens and give the money to the mayor
-	if simpleTax_totalTax != 0 then
-		if oldsalary != 0 then -- Jobs with no salary will get the default message
-			local tax = ((100 - simpleTax_totalTax) / 100) -- Turns value into decimal and inverts it (eg. 25 becoms 0.75)
-			if not ply:isMayor() then
-				local newsalary = math.floor(oldsalary * tax)
-				local taxedcash = math.floor(oldsalary - newsalary)
-				simpleTax_taxToBePaid = simpleTax_taxToBePaid + taxedcash
-				return false, "Payday! You recieved " .. DarkRP.formatMoney(newsalary) .. "! (" .. DarkRP.formatMoney(taxedcash) .. " paid in tax)", newsalary
-			else
-				local mayorpay = oldsalary + simpleTax_taxToBePaid
-				simpleTax_taxToBePaid = 0
-				return false, "Payday! You recieved " .. DarkRP.formatMoney(mayorpay) .. " in tax and salary.", mayorpay
-			end
+	if simpleTax_totalTax >= 1 and oldsalary >= 1 then -- Jobs with no salary will get the default message
+		local tax = ((100 - simpleTax_totalTax) / 100) -- Turns value into decimal and inverts it (eg. 25 becoms 0.75)
+		if not ply:isMayor() then
+			local newsalary = math.floor(oldsalary * tax)
+			local taxedcash = math.floor(oldsalary - newsalary)
+			simpleTax_taxToBePaid = simpleTax_taxToBePaid + taxedcash
+			return false, "Payday! You recieved " .. DarkRP.formatMoney(newsalary) .. "! (" .. DarkRP.formatMoney(taxedcash) .. " paid in tax)", newsalary
+		else
+			local mayorpay = oldsalary + simpleTax_taxToBePaid
+			simpleTax_taxToBePaid = 0
+			return false, "Payday! You recieved " .. DarkRP.formatMoney(mayorpay) .. " in tax and salary.", mayorpay
 		end
 	end
-	if simpleTax_paydaysToNotify >= 1 and not simpleTax_notifyOnCooldown and simpleTax_totalTax != 0 then -- Inform non mayors about /gettax 
-		if not ply:isMayor() then
-			DarkRP.notify(ply, 0, 4, "Use !gettax or /gettax to see the current income tax.")
-			simpleTax_notifyOnCooldown = true
-			timer.Simple(GAMEMODE.Config.paydelay * simpleTax_paydaysToNotify, function() simpleTax_notifyOnCooldown = false end)
-		end
+	if simpleTax_paydaysToNotify >= 1 and not simpleTax_notifyOnCooldown and simpleTax_totalTax >= 1 and not ply:isMayor() then -- Inform non mayors about /gettax
+		DarkRP.notify(ply, 0, 4, "Use !gettax or /gettax to see the current income tax.")
+		simpleTax_notifyOnCooldown = true
+		timer.Simple(GAMEMODE.Config.paydelay * simpleTax_paydaysToNotify, function() simpleTax_notifyOnCooldown = false end)
 	end
 end
 function checkTeamChange(ply,oldteam,newteam) 
